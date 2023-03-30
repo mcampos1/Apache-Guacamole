34578de1ab959f9426c7a9a5b4672b0c96fbb859
Martin Campos <martin.campos8@gmail.com>
2023-03-29 22:27:29 -0400

    Create reset.sh

---
+++ reset.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Stopping containers..."
+docker compose down
+echo ""
+echo "Pruning..."
+docker volume prune
+docker network prune
+echo ""
+echo "This will delete your existing database (./data/)"
+echo "          delete your recordings        (./record/)"
+echo "          delete your drive files       (./drive/)"
+echo "          delete your certs files       (./nginx/ssl/)"
+echo ""
+read -p "Are you sure? " -n 1 -r
+echo ""   # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then # do dangerous stuff
+ chmod -R +x -- ./init
+ sudo rm -r -f ./data/ ./drive/ ./record/ ./nginx/ssl/
+fi
